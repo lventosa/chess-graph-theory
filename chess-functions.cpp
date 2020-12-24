0ab80d27343859cb0653a129074a6e3ddfaf96ab
lventosa <laura.ventosa01@estudiant.upf.edu>
2020-12-24 19:03:00 +0100

    mm

--- chess-functions.cpp
+++ chess-functions.cpp
@@ -25,6 +25,7 @@ graph graph_king(index n, index m) // The king can only move forward one step bu
                 }
             } 
         }
+
         return Kg;
 }
 
@@ -99,4 +100,26 @@ graph graph_knight(index n, index m) // The knight moves one step forward and an
     }
 
     return Kt;
+}
+
+void chess_graph_write(graph& G, index n1, index n2, ofstream& fout)
+{
+
+	vertex vn = G.size();
+
+    edge en = 0; 
+    for(vertex v=0; v<vn; v++){ 
+        en += G[v].size();
+    }  
+    en /= 2;
+     
+    fout << "This -piece- has a total of " << en << " possible final destinations " << endl; // substituir piece pel nom en angles
+    fout << "\nWhere can it get to?" << endl;
+
+    for(vertex v=0; v<vn; v++){
+		fout << "(" << v/n2 << "," << v%n2 << ")" << "\t:";
+        for(index i=0; i<G[v].size(); i++) 
+            fout << "\t" << "(" << G[v][i]/n2 << "," << G[v][i]%n2 << ")"; 
+        fout << endl;
+    }
 }
--- main.cpp
+++ main.cpp
@@ -3,8 +3,35 @@
 int main()
 {
     ofstream fout;
+    int opt;
 
-    fout.open("possible_moves.out");
+    fout.open("possible_moves_chess.out");
+
+    do{
+        cout << "Pick a piece:\n1. King\n2. Bishop\n3. Knight\n4. Rook\n5. Exit" << endl;
+        scanf("%d", &opt); // comprovar que realment es faci aixi
+
+        switch(opt){
+            case 1:
+                graph kg = graph_king(8, 8);
+                chess_graph_write(kg, 8, 8, fout);
+                break;
+
+            case 2:
+                graph bs = graph_bishop(8, 8);
+                // comprovar si el color afecta. Si afecta, preguntar si juga a blanques o negres 
+                chess_graph_write(bs, 8, 8, fout);
+                break;
+
+            case 3:
+                graph kn = graph_knight(8, 8);
+                chess_graph_write(kn, 8, 8, fout);
+                break;
+
+            default:
+                graph rk = graph_rook(8, 8);
+                chess_graph_write(rk, 8, 8, fout);
+    }while(opt!=5);
     
     fout.close();
 
